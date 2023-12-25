69575c59a66fd6dc78e756fee6395e6caf1f413e
Kenneth Long <k.long@imperial.ac.uk>
2023-12-25 00:46:26 +0000

    Partial fix

--- 02-Tests/LhARAFacilityTst.py
+++ 02-Tests/LhARAFacilityTst.py
@@ -16,7 +16,7 @@ import os
 LhARAOpticsPATH    = os.getenv('LhARAOpticsPATH')
 print(LhARAOpticsPATH)
 filename     = os.path.join(LhARAOpticsPATH, \
-                        '11-Parameters/LhARABeamLine-Params-Gauss.csv')
+                        '11-Parameters/LhARABeamLine-Params-LsrDrvn.csv')
 
 ##! Start:
 print("========  LhARAFacility: tests start  ========")
