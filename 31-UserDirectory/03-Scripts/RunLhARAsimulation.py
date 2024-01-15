959d6670eee2cb68bca3b8ace7b83ed985d0c41e
Ken Long <k.long@imperial.ac.uk>
2024-01-15 10:35:53 +0000

    Update to used direvtorty

--- 31-UserDirectory/03-Scripts/RunLhARAsimulation.py
+++ 31-UserDirectory/03-Scripts/RunLhARAsimulation.py
@@ -9,7 +9,7 @@ Test script for "Simulation" class ... simulation processing tasks
 """
 
 import os
-import LIONSimu as Simu
+import Simulation as Simu
 
 ##! Start:
 print("========  Simulation: start  ========")
@@ -17,18 +17,18 @@ print()
 HOMEPATH = os.getenv('HOMEPATH')
 print("HOMEPATH", HOMEPATH)
 filename    = os.path.join(HOMEPATH, \
-                         '11-Parameters/LIONBeamLine-Params-LsrDrvn.csv')
+#                         '11-Parameters/LhARABeamLine-Params-Gauss-Gabor.csv')
+#                         '11-Parameters/LhARABeamLine-Params-Gauss.csv')
+                         '11-Parameters/LhARABeamLine-Params-LsrDrvn.csv')
 datafiledir = os.path.join(HOMEPATH, '99-Scratch')
-Smltn = Simu.Simulation(2000000, filename, datafiledir, 'LIONsimu.dat')
+Smltn = Simu.Simulation(2000000, filename, datafiledir, 'LhARAsimu.dat')
 print()
 print(" <---- Simulation initialised.")
 
 ##! Start:
 print(" ----> Run simulation test:")
 print()
-Smltn.setDebug(True)
 Smltn.RunSim()
-print(Smltn.getNEvt())
 print()
 print(" <---- Simulation test done.")
 
