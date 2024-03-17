649b9fb9a739efedb7bfc5889333f42b73b3e676
Kenneth Long <k.long@imperial.ac.uk>
2024-03-17 22:31:13 +0000

    Partial fiexs

--- 03-Scripts/ReadDRACOsimu.py
+++ 03-Scripts/ReadDRACOsimu.py
@@ -7,6 +7,7 @@ import math as mth
 
 import Particle  as Prtcl
 import BeamLine as BL
+import BeamIO   as bmIO
 
 ##! Start:
 print("========  DRACO event reading: start  ========")
@@ -16,9 +17,11 @@ print("     ----> Initialising with HOMEPATH:", HOMEPATH)
 Debug = False
 
 writePATH = HOMEPATH + "/99-Scratch"
+"""
 ParticleFILE = Prtcl.Particle.openParticleFile(writePATH, \
                                                "DRACOSimulation.dat")
-ibmIOr = bmIO.BeamIO(writePATH, "LIONsimu.dat")
+"""
+ibmIOr = bmIO.BeamIO(writePATH, "DRACOsimulation.dat")
 
 EndOfFile = False
 iEvt = 0
@@ -46,15 +49,15 @@ while not EndOfFile:
         if Debug:
             print(DRACObI)
 
-        else:
-            if not EndOfFile:
-                iEvt += 1
-                if (iEvt % Scl) == 0:
-                    print("         ----> Read event ", iEvt)
-                    iCnt += 1
-                    if iCnt == 10:
-                        iCnt = 1
-                        Scl  = Scl * 10
+    else:
+        if not EndOfFile:
+            iEvt += 1
+            if (iEvt % Scl) == 0:
+                print("         ----> Read event ", iEvt)
+                iCnt += 1
+                if iCnt == 10:
+                    iCnt = 1
+                    Scl  = Scl * 10
                         
 print("     <---- Event loop done, ", iEvt, "events read")
 
