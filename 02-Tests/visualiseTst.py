99d2d6f4157255ed0a5c7f6e6131337734796127
Ken Long <k.long@imperial.ac.uk>
2024-04-28 12:42:35 +0100

    Update vis twst

--- 02-Tests/visualiseTst.py
+++ 02-Tests/visualiseTst.py
@@ -27,8 +27,8 @@ print("========  visualise: tests start  ========")
 HOMEPATH = os.getenv('HOMEPATH')
 inputdatafile = os.path.join(HOMEPATH, \
 #                             '99-Scratch/LION-test.dat')
-                             '99-Scratch/LhARA-Gauss-Gabor-back.dat')
-#                             '11-Parameters/Data4Tests.dat')
+#                             '99-Scratch/LhARA-Gauss-Gabor.dat')
+                             '11-Parameters/Data4Tests.dat')
 
 #.. Open data file and read first record to set up geometry
 ibmIOr = bmIO.BeamIO(None, inputdatafile)
