0c32515b0596f819c84df83dca634fe37236fa94
Ken Long <k.long@imperial.ac.uk>
2024-10-21 22:10:34 +0100

    Commit to test time stamp in commit: 22:10 BST

--- 02-Tests/testGitRepoId.py
+++ 02-Tests/testGitRepoId.py
@@ -3,17 +3,35 @@
 
 import git
 import os
+import datetime as dt
 
 LhARAOpticsPATH = os.getenv('LhARAOpticsPATH')
 repo = git.Repo(LhARAOpticsPATH)
 tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
 print(tags)
 
-head   = repo.head
-master = head.reference
+tagDT = tags[-1].commit.committed_datetime
+print(tagDT, type(tagDT), tagDT.strftime("%Y-%m-%d %H:%M:%S"))
+
+master = repo.head.reference
+print(master)
 log    = master.log()
-Commit = False
 print("Last entry:", log[-1])
+
+entry = log[-1]
+print(entry, type(entry))
+
+entryDT = dt.datetime.fromtimestamp(entry[3][0])
+print(entryDT, type(entryDT), entryDT.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+exit()
+print(log)
+
+
+Commit = False
 print("    Length:", len(log))
+"""
 for iEnt in range(len(log)):
     print(iEnt, log[iEnt])
+"""
