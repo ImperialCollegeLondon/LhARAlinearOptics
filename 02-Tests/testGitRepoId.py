aa0844eab4d96277d897b9302be3206c7fc16fc8
Ken Long <k.long@imperial.ac.uk>
2024-10-21 22:21:00 +0100

    Fix up testGitRepoID

--- 02-Tests/testGitRepoId.py
+++ 02-Tests/testGitRepoId.py
@@ -5,23 +5,25 @@ import git
 import os
 import datetime as dt
 
+print(" Load git repo")
 LhARAOpticsPATH = os.getenv('LhARAOpticsPATH')
 repo = git.Repo(LhARAOpticsPATH)
+
+print(" Get tags, order by time")
 tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
-print(tags)
+
+print(" Most recent tag:", tags[-1])
 
 tagDT = tags[-1].commit.committed_datetime
-print(tagDT, type(tagDT), tagDT.strftime("%Y-%m-%d %H:%M:%S"))
+print(" Date time of most recent tag:", tagDT)
+print('     ----> tagDT.strftime("%Y-%m-%d %H:%M:%S")', \
+      tagDT.strftime("%Y-%m-%d %H:%M:%S"))
 
-master = repo.head.reference
-print(master)
-log    = master.log()
-print("Last entry:", log[-1])
-
-entry = log[-1]
-print(entry, type(entry))
-
-entryDT = dt.datetime.fromtimestamp(entry[3][0])
+log = repo.head.reference.log()
+"""
+print(" Last entry in log:", log[-1])
+"""
+entryDT = dt.datetime.fromtimestamp(log[-1][3][0])
 print(entryDT, type(entryDT), entryDT.strftime("%Y-%m-%d %H:%M:%S"))
 
 
