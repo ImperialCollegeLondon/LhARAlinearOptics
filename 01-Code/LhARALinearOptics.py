0653cd849ad417d14442e87a7d63b0af93855c86
Ken Long <k.long@imperial.ac.uk>
2024-10-22 07:52:43 +0100

    Make version of LhARALinea=rOptics.py to hold utilities

---
+++ 01-Code/LhARALinearOptics.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+LhARALinearOptics utilities:
+============================
+
+Methods:
+ Version(): Returns most recent tag and most recent commit to git repository.
+      Return: List: [ [tagNAME, tagDATETIME], [commitSTRING, commitDATETIME] ]
+
+"""
+
+import git
+import os
+import datetime as dt
+
+def version():
+    Debug = False
+    
+    LLOversion = []
+
+    if Debug:
+        print(" LhARALinearOptics.version start")
+
+    LhARAOpticsPATH = os.getenv('LhARAOpticsPATH')
+    if not os.path.isdir(LhARAOpticsPATH):
+        raise versionException("LhARAOpticsPATH not set")
+
+    repo = git.Repo(LhARAOpticsPATH)
+    if Debug:
+        print("     ----> git repo:", repo)
+
+    tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
+    if Debug:
+        print("     ----> Most recent tag:", tags[-1])
+
+    tagDT = tags[-1].commit.committed_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    if Debug:
+        print("         ----> Date time:", tagDT)
+
+    log     = repo.head.reference.log()
+    entryDT = dt.datetime.fromtimestamp(log[-1][3][0]).strftime("%Y-%m-%d %H:%M:%S")
+    if Debug:
+        print("     ----> Most recent commit:", log[-1][4])
+        print("         ----> Date time:", entryDT)
+
+    LLOversion = [ \
+                   [tags[-1].__str__(),     tagDT], \
+                   [log[-1][4].__str__(), entryDT]  \
+                  ]
+
+    if Debug:
+        print(" <---- LLOversion:", LLOversion)
+    
+    return LLOversion
+
+class versionException(Exception):
+    pass
---
+++ 02-Tests/LhARALinearOpticsTst.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import LhARALinearOptics as LLO
+
+print(" Test LhARALinearOptics.version:")
+print("     ----> version:", LLO.version())
+
